437f263360e20fb22c74d17b6fc8ba660c5f7309
Heikki Leivo <heikki.leivo@phnet.fi>
2018-12-30 18:45:08 +0200

    Preliminary implementation of collect-images command

--- sunset.py
+++ sunset.py
@@ -5,6 +5,7 @@ import argparse
 import shutil
 from datetime import datetime
 from calc import Suncalc, EVENTS
+from glob import glob
 
 LOG_FORMAT="%(message)s"
 
@@ -49,6 +50,36 @@ def show_time(args):
 
     print("Local {} is at {}".format(args.event, value))
 
+def collect_images(args):
+
+    from settings import TARGET_DIRECTORY as source_dir # sic!
+    if os.path.isdir(source_dir):
+        if os.path.isdir(args.target) == False:
+            os.makedirs(args.target)
+        if args.purge:
+            g = glob(os.path.join(args.target, "*"))
+            count = len(g)
+            if count > 0: 
+                if args.silent == False:
+                    print("All files in target directory {} will be deleted.")
+                    print("Do you want to continue? (y/n)")
+                    if input().lower() != 'y': 
+                        print("Operation cancelled.")
+                        return
+                for file in g:
+                    os.remove(file)
+                print("Successfully created {} symlinks to {}.".format(count, args.target))
+            else:
+                print("No files found to be processed.")
+
+        pattern = os.path.join(source_dir, args.subdir, "*{:+d}.jpg".format(args.offset))
+        files = sorted(glob(pattern), key=os.path.getmtime)
+        for index, path in enumerate(files):
+            os.symlink(path, os.path.join(args.target, "{:d}.jpg".format(index)))
+            
+    else:
+        print("Source directory defined in settings does not exist.".format(TARGET_DIRECTORY))
+
 def main():
     parser = argparse.ArgumentParser("Sunset Calculator")
     subparsers = parser.add_subparsers(help="Action to perfom.")
@@ -66,7 +97,24 @@ def main():
     subparser.set_defaults(func=run_commands)
     subparser.add_argument('--execute', type=bool, 
         default=False, help="If set to True, execute commands using os.system.")
+
+    # Define parser for collect-images action
+    subparser = subparsers.add_parser("collect-images", 
+        help="Collect images for creating time-lapse video.")
+    subparser.set_defaults(func=collect_images)
+    subparser.add_argument('--offset', type=int, required=True, 
+        help="Index of images to be collected.")
+    subparser.add_argument('--target', type=str, required=True,
+        help="Target path for images.")
+    subparser.add_argument('--subdir', type=str, required=False, default='', 
+        help="Sub-directory to look for images.")
+    subparser.add_argument('--purge', type=bool, default=False, 
+        help="If set to True, remove old files before proceeding.")
+    subparser.add_argument('--silent', type=bool, default=False, 
+        help="If set to True, do not ask for confirmation.")
+
     args = parser.parse_args()
+
 
     if hasattr(args, 'func') == False:
         parser.print_help()
